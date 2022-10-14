148828229b98e582250107cdccc8269991a66849
Mykola25 <114308545+Mykola25@users.noreply.github.com>
2022-10-14 22:46:45 +0300

    Звіт 3

---
+++ OP_Lab_3/Task3/Task3.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float a1 = 1000;
+	double b1 = 0.0001, x1;
+	double a2 = 1000, x2;
+	float b2 = 0.0001;
+	x1 = (pow(a1 - b1, 2) - (a1 * a1 - 2 * a1 * b1)) / (b1 * b1);
+	printf("Вираз 1 = %.15f\n", x1);
+	x2 = (pow(a2 - b2, 2) - (a2 * a2 - 2 * a2 * b2)) / (b2 * b2);
+	printf("Вираз 2 = %.15f\n", x2);
+	return 0;
+}
---
+++ OP_Lab_3/Task4/Task4.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int m, n, r;
+    printf("Введіть число m: ");
+    scanf_s("%d", &m);
+    printf("Введіть число n: ");
+    scanf_s("%d", &n);
+    printf("m = %d, n = %d\n", m, n);
+    r = m - ++n;
+    printf("1) m - ++n = %d\n", r);
+    printf("m = %d, n = %d\n", m, n);
+    r = ++m > --n;
+    printf("2) ++m > --n = %d\n", r);
+    printf("n = %d, m = %d\n", n, m);
+    r = --n < ++m;
+    printf("3) --n < ++m = %d\n", r);
+    printf("n = %d, m = %d\n", n, m);
+    r = ++n * ++m;
+    printf("4) ++n * ++m = %d\n", r);
+    printf("n = %d, m = %d\n", n, m);
+    r = ++n * m++;
+    printf("5) ++n * m++ = %d\n", r);
+    printf("n = %d, m = %d\n", n, m);
+    r = n * ++m;
+    printf("6) n * ++m = %d\n", r);
+    return 0;
+}
