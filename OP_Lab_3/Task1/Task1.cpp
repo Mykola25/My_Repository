79f87e66cdcca4ba22c0d7fc54d1d4aa50b05f5a
Mykola25 <114308545+Mykola25@users.noreply.github.com>
2022-10-23 12:58:16 +0300

    Laba3

---
+++ OP_Lab_3/Task1/Task1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <cstdlib>
+
+int main()
+{
+	system("color 2F");
+	int x = 5, y = 6;
+	printf("\n|%6d%d%d%d%d%-6d|\n", x, x, x, y, y, y);
+	printf("|%d%d%d%11d%d%d|\n", x, x, x, y, y, y);
+	printf("|%6d%d%d%d%d%-6d|\n", x, x, x, y, y, y);
+	printf("|%d%d%d%11d%d%d|\n", x, x, x, y, y, y);
+	return 0;
+}
+
---
+++ OP_Lab_3/Task3/Task3.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float a1 = 1000, b1 = 0.0001, x1, y1, s1;
+	printf("Обчислення при float:\n");
+	x1 = (pow((a1 - b1), 2)) - (pow(a1, 2) - 2 * a1 * b1);
+	y1 = pow(b1, 2);
+	s1 = (x1) / (y1);
+	printf("Результат = %f\n", s1);
+	printf("\nОбчислення при double:\n");
+	double a2 = 1000, b2 = 0.0001, x2, y2, s2;
+	x2 = (pow((a2 - b2), 2)) - (pow(a2, 2) - 2 * a2 * b2);
+	y2 = pow(b2, 2);
+	s2 = (x2) / (y2);
+	printf("Результат = %f\n", s2);
+	printf("\nРізниця = %f\n", s2 - s1);
+	return 0;
+}
